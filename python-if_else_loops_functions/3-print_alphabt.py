b544835af4f0dc1cf027d5018d4a0664743a7eac
root <6111@holbertonstudents.com>
2023-05-10 12:16:30 -0700

    fix long line

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-alphabet = ''.join([chr(i) for i in range(97, 123) if chr(i) not in ('q', 'e')])
+alphabet = ''.join(
+    [chr(i) for i in range(97, 123) if chr(i) not in ('q', 'e')]
+)
 print("{}".format(alphabet), end="")
