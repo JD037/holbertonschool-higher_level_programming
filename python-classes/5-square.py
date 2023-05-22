f5b88f5ea00c4d5c52724aaf21412b4900da1df3
root <6111@holbertonstudents.com>
2023-05-22 09:46:00 -0700

    add my_print method

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -27,7 +27,7 @@ class Square:
         """Set the size of the square
 
         Args:
-            value (int): The new size of the square.
+            value (int): The new size of the square.1~
 
         Raises:
             TypeError: If value is not an integer.
@@ -43,3 +43,11 @@ class Square:
     def area(self):
         """Return the current square area"""
         return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the # character"""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
