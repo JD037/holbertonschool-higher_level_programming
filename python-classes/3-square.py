a8206e20f0c3e69602d9b786b08a0dfc8e6c722b
root <6111@holbertonstudents.com>
2023-05-22 09:28:13 -0700

    add public instance method

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module to define a square"""
+
+
+class Square:
+    """A class that defines a square by size"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+    Args:
+        size (int): The size of the new square.~
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = size
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2
