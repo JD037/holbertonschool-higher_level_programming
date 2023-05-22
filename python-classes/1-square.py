f77589a30a3115bc2324ec6463846e92de2eacf6
root <6111@holbertonstudents.com>
2023-05-22 09:16:59 -0700

    add init method to class

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module to define a square"""
+
+
+
+class Square:
+    """A class that defines a square by size"""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.~
+        """
+        self.__size = size
