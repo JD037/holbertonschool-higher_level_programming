7f8f0da2b51d4fa06d7dcc56dde5f68721d0b2a1
root <6111@holbertonstudents.com>
2023-05-22 10:29:31 -0700

    fixed size var

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# Write class square defing a square: 
-"""Module to define a Square"""
-
+"""Module to define a square"""
 
 class Square:
     """A class that defines a square by size"""
@@ -9,19 +7,19 @@ class Square:
     def __init__(self, size=0):
         """Initialize a new Square.
 
-    Args:
-        size (int): The size of the new square.
+        Args:
+            size (int): The size of the new square.
 
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    elif size < 0:
-        raise ValueError("size must be >= 0")
-    else:
-        self.__size = size
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
         """Return the current square area"""
