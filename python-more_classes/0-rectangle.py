eaf170a4f6f58f688f529437c2ba1b00b366c51a
root <6111@holbertonstudents.com>
2023-05-23 13:25:46 -0700

    add line before class

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -5,6 +5,7 @@ This is the "0-rectangle" module.
 The 0-rectangle module defines the Rectangle class.
 """
 
+
 class Rectangle:
     """This is the Rectangle class. It is an empty class."""
     pass
