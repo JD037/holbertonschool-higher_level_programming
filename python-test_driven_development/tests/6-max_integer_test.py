07c9f28ff54de7051cfc2c47698c7cfca895542c
root <6111@holbertonstudents.com>
2023-05-23 11:29:30 -0700

    rework

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,42 +1,18 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
+"""Module to find the max integer in a list
 """
 
 
-import unittest
-
-
-from 6-max_integer import max_integer
-
-class TestMaxInteger(unittest.TestCase):
-    def test_max_at_end(self):
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-
-    def test_max_at_beginning(self):
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
-
-    def test_max_in_middle(self):
-        self.assertEqual(max_integer([1, 4, 3, 2]), 4)
-
-    def test_one_negative(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-
-    def test_all_negative(self):
-        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-
-    def test_one_element(self):
-        self.assertEqual(max_integer([1]), 1)
-
-    def test_empty_list(self):
-        self.assertEqual(max_integer([]), None)
-
-    def test_not_a_list(self):
-        with self.assertRaises(TypeError):
-            max_integer("This is not a list")
-
-    def test_list_of_strings(self):
-        with self.assertRaises(TypeError):
-            max_integer(["This", "is", "a", "list", "of", "strings"])
-
-if __name__ == "__main__":
-    unittest.main()
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
+    if len(list) == 0:
+        return None
+    result = list[0]
+    i = 1
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
+        i += 1
+    return result
