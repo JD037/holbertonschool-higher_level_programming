dbc32aaca8d8a60b4652feb7b8f447449d6d04d1
root <6111@holbertonstudents.com>
2023-05-23 13:24:51 -0700

    add doc

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This is the "0-rectangle" module.
+
+The 0-rectangle module defines the Rectangle class.
+"""
+
 class Rectangle:
+    """This is the Rectangle class. It is an empty class."""
     pass
