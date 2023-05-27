70814fd95c1da108ee6391054bbfa09ed3efa738
root <6111@holbertonstudents.com>
2023-05-27 15:02:43 -0700

    class and test creation

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module contains the class MyList.
+"""
+
+
+class MyList(list):
+    """
+    Class MyList that inherits from list.
+    """
+
+    def print_sorted(self):
+        """
+        Prints the list, but sorted (ascending sort).
+        """
+        print(sorted(self))
