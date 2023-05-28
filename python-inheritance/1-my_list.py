4b9cd308944bb031c8eee6ba5b935a9b7992cb95
root <6111@holbertonstudents.com>
2023-05-27 22:12:42 -0700

    change docstrings

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,12 +4,7 @@
 
 
 class MyList(list):
-    """
-    Class MyList that inherits from list.
-    """
-
+    """Class MyList that inherits from list."""
     def print_sorted(self):
-        """
-        Prints the list, but sorted (ascending sort).
-        """
+        """Prints the list, but sorted (ascending sort)."""
         print(sorted(self))
