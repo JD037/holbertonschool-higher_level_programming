a1c5bd2ee2b15701c08f80ff53b7ce0274d63456
root <6111@holbertonstudents.com>
2023-06-04 13:57:38 -0700

    documentation

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,7 +1,24 @@
+#!/usr/bin/python3
+"""
+This module contains the Base class
+This class will be the base of all other classes in the project.
+The goal of it is to manage id attribute in all your future classes
+"""
+
+
 class Base:
+    """
+    Base class
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        Constructor for Base class
+
+        Parameters:
+        id (int): id of the Base object, defaults to None
+        """
         if id is not None:
             self.id = id
         else:
