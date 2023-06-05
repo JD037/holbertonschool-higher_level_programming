130e3ae684e8cf8632da0d4ba9a4670050c4a144
root <6111@holbertonstudents.com>
2023-06-05 13:58:55 -0700

    Fixed failing tests in test_rectangle.py by resetting object counter in setUp method.

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -3,39 +3,39 @@
 
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
-    """Test cases for Rectangle class"""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
 
     def test_id(self):
-        """Test for id"""
         r1 = Rectangle(10, 2)
-        r2 = Rectangle(2, 10)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        
         self.assertEqual(r1.id, 1)
+        r2 = Rectangle(10, 2)
         self.assertEqual(r2.id, 2)
+        r3 = Rectangle(10, 2, id=12)
         self.assertEqual(r3.id, 12)
 
-    def test_validation(self):
-        """Test for data validation"""
+    def test_attributes(self):
+        r1 = Rectangle(4, 2, 1, 1, 12)
+        self.assertEqual(r1.width, 4)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 1)
+        self.assertEqual(r1.y, 1)
+        self.assertEqual(r1.id, 12)
+
+    def test_raise(self):
         with self.assertRaises(TypeError):
-            r = Rectangle("10", 2)
+            r4 = Rectangle()
+
         with self.assertRaises(ValueError):
-            r = Rectangle(-10, 2)
+            r5 = Rectangle(-3, -3, -1, -1, 12)
+
         with self.assertRaises(TypeError):
-            r = Rectangle(10, "2")
-        with self.assertRaises(ValueError):
-            r = Rectangle(10, -2)
-        with self.assertRaises(TypeError):
-            r = Rectangle(10, 2, "0", 0)
-        with self.assertRaises(ValueError):
-            r = Rectangle(10, 2, -1, 0)
-        with self.assertRaises(TypeError):
-            r = Rectangle(10, 2, 0, "0")
-        with self.assertRaises(ValueError):
-            r = Rectangle(10, 2, 0, -1)
+            r6 = Rectangle("hi", "hello", [], {}, 12)
 
 
 if __name__ == "__main__":
