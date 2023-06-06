3648191500867b199b54bd8efc15fd5097fff561
root <6111@holbertonstudents.com>
2023-06-06 11:20:15 -0700

    add return dict method to square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -44,3 +44,12 @@ class Square(Rectangle):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Square"""
+        return {
+            'id': self.id,
+            'size': self.width,  # or self.height
+            'x': self.x,
+            'y': self.y
+        }
