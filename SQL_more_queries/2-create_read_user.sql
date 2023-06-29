e4f6d3c1d1c0e5ae4a7458b15dfd710a6c391f7c
Josh D <joj237@gmail.com>
2023-06-28 19:35:30 -0700

    Create read-only user and database

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,7 @@
 -- 2-create_read_user.sql
 
--- Create the database hbtn_0d_2 if it does not exist
+-- creates the database hbtn_0d_2 and the user user_0d_2 yes
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-
--- Create the user 'user_0d_2' if they do not exist, and grant SELECT privilege on hbtn_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
 FLUSH PRIVILEGES;
