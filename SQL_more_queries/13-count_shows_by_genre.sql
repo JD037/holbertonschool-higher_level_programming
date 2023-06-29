20cd0d3d07db28dff71e97d123f8ed1ad27999bf
Josh D <joj237@gmail.com>
2023-06-28 20:02:34 -0700

    Corrected the table name in SQL query to match the ones provided in the database dump

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,9 +2,10 @@
 
 -- Script to list all genres and the number of shows linked to each
 
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING COUNT(tv_show_genres.show_id) > 0
-ORDER BY COUNT(tv_show_genres.show_id) DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_shows.title) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY genre
+HAVING COUNT(tv_shows.title) > 0
+ORDER BY number_of_shows DESC;
