53ad7181045a49ce6beb5f181c4cd0a55c92c7e0
Josh D <joj237@gmail.com>
2023-06-28 19:33:31 -0700

    forgot task 1 was broken

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- 2-create_read_user.sql
+
+-- Create the database hbtn_0d_2 if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user 'user_0d_2' if they do not exist, and grant SELECT privilege on hbtn_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
