217b4b886a87ec22c1fd4fe9ad00afd84aa38a9f
Josh D <joj237@gmail.com>
2023-06-30 15:14:18 -0700

    fix pycode errors in model state file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,13 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Class representing the states table.
-    
+
     Attributes:
         id (sqlalchemy.Column): The state's id.
         name (sqlalchemy.Column): The state's name.
     """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement="auto")
+    id = Column(Integer,
+                primary_key=True,
+                nullable=False,
+                autoincrement="auto")
     name = Column(String(128), nullable=False)
